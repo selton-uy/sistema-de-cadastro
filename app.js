026dedb03f09b010b26ddee739581f472a9b66bf
Selton <selton.bae@gmail.com>
2023-01-08 17:42:26 -0300

    console log indicando modificações do paulo

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Aplicação está se conectando ao banco de dados");
+//mais um console log de teste:
+console.log("O Paulo fez esse commit sem voce ver na aula!");
+
