5e17ca7135294ea42b0859e52db2a84d83c316b7
Selton <selton.bae@gmail.com>
2023-01-08 17:45:29 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
 //mais um console log de teste:
 console.log("O Paulo fez esse commit sem voce ver na aula!");
-
+console.log("Enviando para o GitHub");
